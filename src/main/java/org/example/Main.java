a54c66785ecd69941f4859e6576f4a1c8b21b4ec
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-08 20:46:53 +0330

    Main edited. + Image.java deleted.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.utility.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.menu();
+    }
+}
