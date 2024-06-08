f0a1504a08831e1c59196a833a29768d6b099b08
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-08 20:15:49 +0330

    ExpertRepository class + interface created.

---
+++ src/main/java/org/example/repository/expert/ExpertRepository.java
@@ -0,0 +1,13 @@
+package org.example.repository.expert;
+
+import org.example.base.repository.BaseRepository;
+import org.example.model.Expert;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ExpertRepository extends BaseRepository<Expert, Long> {
+    Optional<Expert> findByUsername(String username);
+
+    Optional<List<Expert>> loadAllWaitingforVerificationExperts();
+}
---
+++ src/main/java/org/example/repository/expert/ExpertRepositoryImpl.java
@@ -0,0 +1,47 @@
+package org.example.repository.expert;
+
+import org.example.base.repository.BaseRepositoryImpl;
+import org.example.exception.NoResultException;
+import org.example.model.Customer;
+import org.example.model.Expert;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ExpertRepositoryImpl extends BaseRepositoryImpl<Expert, Long>
+        implements ExpertRepository {
+    public ExpertRepositoryImpl(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    @Override
+    public Class<Expert> getClassName() {
+        return Expert.class;
+    }
+
+    @Override
+    public Optional<Expert> findByUsername(String username) {
+        Session session = sessionFactory.getCurrentSession();
+        String hql = "FROM Expert e WHERE e.username = :username";
+        Query<Expert> query = session.createQuery(hql, getClassName())
+                .setParameter("username", username);
+        List<Expert> resultList = query.getResultList();
+        if (resultList.isEmpty())
+            return Optional.empty();
+        return Optional.of(resultList.get(0));
+    }
+
+    @Override
+    public Optional<List<Expert>> loadAllWaitingforVerificationExperts() {
+        Session session = sessionFactory.getCurrentSession();
+        String hql = "FROM Expert e where e.expertsLevel = 'WAITING_FOR_VERIFYING' ";
+        Query<Expert> query = session.createQuery(hql, getClassName());
+        List<Expert> experts = query.getResultList();
+        if (experts.isEmpty())
+            return Optional.empty();
+        return Optional.of(experts);
+    }
+}
