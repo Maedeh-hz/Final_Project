5a5d28240b64684a6c39ef2f63e508892a2e5a46
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-08 20:41:38 +0330

    Custom Exceptions created.

---
+++ src/main/java/org/example/exception/NoResultException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class NoResultException extends RuntimeException{
+    public NoResultException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/example/exception/UnverifiedExpertException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class UnverifiedExpertException extends RuntimeException{
+    public UnverifiedExpertException(String message) {
+        super(message);
+    }
+}
