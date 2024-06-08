354e33268172006b708d28e9a79b7092814abe74
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-08 20:44:21 +0330

    SecurityContext created.

---
+++ src/main/java/org/example/utility/SecurityContext.java
@@ -0,0 +1,13 @@
+package org.example.utility;
+
+import org.example.model.Admin;
+import org.example.model.Customer;
+import org.example.model.Expert;
+import org.example.model.User;
+
+public class SecurityContext {
+    public static User user;
+    public static Expert expert;
+    public static Customer customer;
+    public static Admin admin;
+}
