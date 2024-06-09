14f3823d887716c44b652a51fb923ed2daf59104
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-10 02:49:24 +0330

    OrdersLevel enum created.

---
+++ src/main/java/org/example/model/enums/OrdersLevel.java
@@ -0,0 +1,10 @@
+package org.example.model.enums;
+
+public enum OrdersLevel {
+    WAITING_FOR_EXPERT_SUGGESTION,
+    WAITING_FOR_EXPERT_TO_TAKE,
+    WAITING_FOR_EXPERT_TO_VISIT,
+    STARTED,
+    DONE,
+    PAYED
+}
