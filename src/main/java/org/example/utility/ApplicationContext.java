46f37596ada9594f914a341f7e8bc3410465f73d
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-09 17:32:43 +0330

    ApplicationContext edited.

--- src/main/java/org/example/utility/ApplicationContext.java
+++ src/main/java/org/example/utility/ApplicationContext.java
@@ -17,6 +17,8 @@ import org.example.repository.suggestion.SuggestionRepository;
 import org.example.repository.suggestion.SuggestionRepositoryImpl;
 import org.example.repository.user.UserRepository;
 import org.example.repository.user.UserRepositoryImpl;
+import org.example.repository.users_subservice.User_SubServiceRepository;
+import org.example.repository.users_subservice.User_SubServiceRepositoryImpl;
 import org.example.repository.wallet.WalletRepository;
 import org.example.repository.wallet.WalletRepositoryImpl;
 import org.example.service.admin.AdminService;
@@ -29,12 +31,14 @@ import org.example.service.order.OrderService;
 import org.example.service.order.OrderServiceImpl;
 import org.example.service.service.ServiceService;
 import org.example.service.service.ServiceServiceImpl;
-import org.example.service.subservice.SubserviceService;
+import org.example.service.subservice.Sub_serviceService;
 import org.example.service.subservice.SubserviceServiceImpl;
 import org.example.service.suggestion.SuggestionService;
 import org.example.service.suggestion.SuggestionServiceImpl;
 import org.example.service.user.UserService;
 import org.example.service.user.UserServiceImpl;
+import org.example.service.users_subservice.User_SubServiceService;
+import org.example.service.users_subservice.User_SubServiceServiceImpl;
 import org.example.service.wallet.WalletService;
 import org.example.service.wallet.WalletServiceImpl;
 import org.hibernate.SessionFactory;
@@ -48,6 +52,7 @@ public class ApplicationContext {
     private static final OrderRepository ORDER_REPOSITORY;
     private static final ServiceRepository SERVICE_REPOSITORY;
     private static final SubserviceRepository SUBSERVICE_REPOSITORY;
+    private static final User_SubServiceRepository USERS_SUB_SERVICE_REPOSITORY;
     private static final SuggestionRepository SUGGESTION_REPOSITORY;
     private static final WalletRepository WALLET_REPOSITORY;
     private static final UserService USER_SERVICE;
@@ -56,7 +61,8 @@ public class ApplicationContext {
     private static final ExpertService EXPERT_SERVICE;
     private static final OrderService ORDER_SERVICE;
     private static final ServiceService SERVICE_SERVICE;
-    private static final SubserviceService SUBSERVICE_SERVICE;
+    private static final Sub_serviceService SUBSERVICE_SERVICE;
+    private static final User_SubServiceService USERS_SUB_SERVICE_SERVICE;
     private static final SuggestionService SUGGESTION_SERVICE;
     private static final WalletService WALLET_SERVICE;
 
@@ -69,6 +75,7 @@ public class ApplicationContext {
         ORDER_REPOSITORY = new OrderRepositoryImpl(SESSION_FACTORY);
         SERVICE_REPOSITORY = new ServiceRepositoryImpl(SESSION_FACTORY);
         SUBSERVICE_REPOSITORY = new SubserviceRepositoryImpl(SESSION_FACTORY);
+        USERS_SUB_SERVICE_REPOSITORY = new User_SubServiceRepositoryImpl(SESSION_FACTORY);
         SUGGESTION_REPOSITORY = new SuggestionRepositoryImpl(SESSION_FACTORY);
         WALLET_REPOSITORY = new WalletRepositoryImpl(SESSION_FACTORY);
         USER_SERVICE = new UserServiceImpl(USER_REPOSITORY, SESSION_FACTORY);
@@ -78,6 +85,7 @@ public class ApplicationContext {
         ORDER_SERVICE = new OrderServiceImpl(ORDER_REPOSITORY, SESSION_FACTORY);
         SERVICE_SERVICE = new ServiceServiceImpl(SERVICE_REPOSITORY, SESSION_FACTORY);
         SUBSERVICE_SERVICE = new SubserviceServiceImpl(SUBSERVICE_REPOSITORY, SESSION_FACTORY);
+        USERS_SUB_SERVICE_SERVICE = new User_SubServiceServiceImpl(USERS_SUB_SERVICE_REPOSITORY, SESSION_FACTORY);
         SUGGESTION_SERVICE = new SuggestionServiceImpl(SUGGESTION_REPOSITORY, SESSION_FACTORY);
         WALLET_SERVICE = new WalletServiceImpl(WALLET_REPOSITORY, SESSION_FACTORY);
     }
@@ -106,8 +114,12 @@ public class ApplicationContext {
         return SERVICE_SERVICE;
     }
 
-    public static SubserviceService getSubserviceService() {
+    public static Sub_serviceService getSubserviceService() {
         return SUBSERVICE_SERVICE;
+    }
+
+    public static User_SubServiceService getUsersSubServiceService(){
+        return USERS_SUB_SERVICE_SERVICE;
     }
 
     public static SuggestionService getSuggestionService() {
