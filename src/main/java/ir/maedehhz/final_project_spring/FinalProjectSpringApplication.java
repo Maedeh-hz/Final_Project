aacf90f5f8eef9d822d6f538a85285b0de31b440
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-15 11:47:25 +0330

    Phase 2: started. (spring boot)

---
+++ src/main/java/ir/maedehhz/final_project_spring/FinalProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package ir.maedehhz.final_project_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/maedehhz/final_project_spring/FinalProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maedehhz.final_project_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
