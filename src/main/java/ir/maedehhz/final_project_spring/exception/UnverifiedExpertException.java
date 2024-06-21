e05f30d9c38732882230d7ba1ebd5030eaca3a55
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-21 09:23:27 +0330

    1 custom exception created. (UnverifiedExpert)

---
+++ src/main/java/ir/maedehhz/final_project_spring/exception/UnverifiedExpertException.java
@@ -0,0 +1,7 @@
+package ir.maedehhz.final_project_spring.exception;
+
+public class UnverifiedExpertException extends RuntimeException{
+    public UnverifiedExpertException(String message) {
+        super(message);
+    }
+}
