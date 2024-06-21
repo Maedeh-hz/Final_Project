5d5b90ef4927c838c363e882599fba304d3dc7c9
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-21 08:52:14 +0330

    AdminRepository created.

---
+++ src/main/java/ir/maedehhz/final_project_spring/repository/AdminRepository.java
@@ -0,0 +1,11 @@
+package ir.maedehhz.final_project_spring.repository;
+
+import ir.maedehhz.final_project_spring.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    <S extends Admin> boolean existsByUsername(String username);
+}
