dea715f92a083d1f4a88dd47615231ffbcf4b10a
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-21 09:18:19 +0330

    1 custom exception created. (SameInfo)

---
+++ src/main/java/ir/maedehhz/final_project_spring/exception/SameInfoException.java
@@ -0,0 +1,7 @@
+package ir.maedehhz.final_project_spring.exception;
+
+public class SameInfoException extends RuntimeException{
+    public SameInfoException(String message) {
+        super(message);
+    }
+}
