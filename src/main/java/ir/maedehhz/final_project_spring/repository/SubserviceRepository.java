49254e2bfe323a35d680bec91c50ea734234332f
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-21 10:32:53 +0330

    SubserviceRepository updated.

--- src/main/java/ir/maedehhz/final_project_spring/repository/SubserviceRepository.java
+++ src/main/java/ir/maedehhz/final_project_spring/repository/SubserviceRepository.java
@@ -1,12 +1,18 @@
 package ir.maedehhz.final_project_spring.repository;
 
 import ir.maedehhz.final_project_spring.model.Subservice;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SubserviceRepository extends JpaRepository<Subservice, Long> {
 
     <S extends Subservice> boolean existsByName(String name);
     <S extends Subservice, R> R findByName(String name);
+
+    @Override
+    List<Subservice> findAll();
 }
