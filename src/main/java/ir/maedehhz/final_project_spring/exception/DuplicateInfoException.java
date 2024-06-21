c6acb469ea4a4357cd5a8a716f9751b971de144e
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-21 08:53:30 +0330

    two custom exceptions created. (DuplicateInfo + NotFound )

---
+++ src/main/java/ir/maedehhz/final_project_spring/exception/DuplicateInfoException.java
@@ -0,0 +1,7 @@
+package ir.maedehhz.final_project_spring.exception;
+
+public class DuplicateInfoException extends RuntimeException{
+    public DuplicateInfoException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ir/maedehhz/final_project_spring/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package ir.maedehhz.final_project_spring.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
