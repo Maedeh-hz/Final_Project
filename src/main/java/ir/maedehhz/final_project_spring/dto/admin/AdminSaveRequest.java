62e3c178fa9a456086cd87bfe2da93978d18f726
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-30 21:21:56 +0330

    Admin DTOs updated.

--- src/main/java/ir/maedehhz/final_project_spring/dto/admin/AdminSaveRequest.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/admin/AdminSaveRequest.java
@@ -2,8 +2,6 @@ package ir.maedehhz.final_project_spring.dto.admin;
 
 import ir.maedehhz.final_project_spring.model.Wallet;
 
-import java.time.LocalDate;
-
 public record AdminSaveRequest(String firstName, String lastName,
                                String email, String username, String password,
                                Wallet wallet){
--- src/main/java/ir/maedehhz/final_project_spring/dto/admin/AdminSaveResponse.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/admin/AdminSaveResponse.java
@@ -3,7 +3,6 @@ package ir.maedehhz.final_project_spring.dto.admin;
 import ir.maedehhz.final_project_spring.model.Wallet;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 public record AdminSaveResponse(long id, String firstName, String lastName, String email, String username,
                                 Wallet wallet, LocalDate registrationDate) {
