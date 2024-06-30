073ec470a7e23dc4a13eae39490c991462fe96e2
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-30 20:33:15 +0330

    CommentRepository updated.

--- src/main/java/ir/maedehhz/final_project_spring/repository/CommentRepository.java
+++ src/main/java/ir/maedehhz/final_project_spring/repository/CommentRepository.java
@@ -1,9 +1,14 @@
 package ir.maedehhz.final_project_spring.repository;
 
 import ir.maedehhz.final_project_spring.model.Comment;
+import ir.maedehhz.final_project_spring.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<Comment> findByOrder(Order order);
+    boolean existsByOrder(Order order);
 }
