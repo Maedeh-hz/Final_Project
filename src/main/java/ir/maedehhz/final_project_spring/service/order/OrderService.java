9369d00cb65c70b5d5adbd25184907535dc05549
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-30 20:56:40 +0330

    OrderService updated.

--- src/main/java/ir/maedehhz/final_project_spring/service/order/OrderService.java
+++ src/main/java/ir/maedehhz/final_project_spring/service/order/OrderService.java
@@ -1,14 +1,20 @@
 package ir.maedehhz.final_project_spring.service.order;
 
 import ir.maedehhz.final_project_spring.model.Order;
+import ir.maedehhz.final_project_spring.model.enums.OrderStatus;
+
+import java.util.List;
 
 public interface OrderService {
-    Order save(Order order);
+    Order save(Order order, long subserviceId, long customerId);
     Order findById(long id);
-    Order updateStatusToWaitingForExpertToTake(long orderId);
+    Order updatingOrderSuggestion(Order order);
     Order updateStatusToWaitingForExpertToVisit(long orderId);
+    List<Order> findAllByExpertsSubservice(long expertId);
+    List<Order> findAllByStatusWaitingForSuggestion();
     Order updateStatusToStarted(long orderId);
     Order updateStatusToDone(long orderId);
     Order updateStatusToPayed(long orderId);
+    void reduce1ScoreFromExpertPerHour(long orderId);
 
 }
--- src/main/java/ir/maedehhz/final_project_spring/service/order/OrderServiceImpl.java
+++ src/main/java/ir/maedehhz/final_project_spring/service/order/OrderServiceImpl.java
@@ -1,25 +1,50 @@
 package ir.maedehhz.final_project_spring.service.order;
 
+import ir.maedehhz.final_project_spring.exception.IneligibleObjectException;
 import ir.maedehhz.final_project_spring.exception.InvalidDateForOrderException;
 import ir.maedehhz.final_project_spring.exception.NotFoundException;
-import ir.maedehhz.final_project_spring.model.Order;
+import ir.maedehhz.final_project_spring.model.*;
 import ir.maedehhz.final_project_spring.model.enums.OrderStatus;
 import ir.maedehhz.final_project_spring.repository.OrderRepository;
+import ir.maedehhz.final_project_spring.service.customer.CustomerServiceImpl;
+import ir.maedehhz.final_project_spring.service.expert.ExpertServiceImpl;
+import ir.maedehhz.final_project_spring.service.subservice.SubserviceServiceImpl;
+import ir.maedehhz.final_project_spring.service.user_subservice.User_SubserviceServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final OrderRepository repository;
+
+    private final ExpertServiceImpl expertService;
+
+    private final SubserviceServiceImpl subserviceService;
+
+    private final CustomerServiceImpl customerService;
+
+    private final User_SubserviceServiceImpl userSubserviceService;
+
     @Override
-    public Order save(Order order) {
+    public Order save(Order order, long subserviceId, long customerId) {
+        Subservice subservice = subserviceService.findById(subserviceId);
+        Customer customer = customerService.findById(customerId);
+        order.setCustomer(customer);
+        order.setSubservice(subservice);
+
         if (order.getToDoDateAndTime().isBefore(LocalDateTime.now()))
             throw new InvalidDateForOrderException("The entered date is before today!");
 
+        order.setRegisterDate(LocalDate.now());
+        order.setStatus(OrderStatus.WAITING_FOR_EXPERT_SUGGESTION);
         return repository.save(order);
     }
 
@@ -30,10 +55,8 @@ public class OrderServiceImpl implements OrderService{
     }
 
     @Override
-    public Order updateStatusToWaitingForExpertToTake(long orderId) {
-        Order byId = findById(orderId);
-        byId.setStatus(OrderStatus.WAITING_FOR_EXPERT_TO_TAKE);
-        return repository.save(byId);
+    public Order updatingOrderSuggestion(Order order) {
+        return repository.save(order);
     }
 
     @Override
@@ -44,10 +67,41 @@ public class OrderServiceImpl implements OrderService{
     }
 
     @Override
+    public List<Order> findAllByExpertsSubservice(long expertId) {
+        Expert expert = expertService.findById(expertId);
+
+        List<User_SubService> allByExpertId = userSubserviceService.findAllByExpert_Id(expert.getId());
+        List<Subservice> subservices = new ArrayList<>();
+        allByExpertId.forEach(userSubService ->
+                subservices.add(userSubService.getSubservice()));
+
+        List<Order> orders = new ArrayList<>();
+        for (Subservice subservice : subservices) {
+            orders.addAll(repository.findAllBySubservice(subservice));
+        }
+        if (orders.isEmpty())
+            throw new NotFoundException("No order found for your specialist!");
+
+        return orders;
+    }
+
+    @Override
+    public List<Order> findAllByStatusWaitingForSuggestion() {
+        if (!repository.existsByStatus(OrderStatus.WAITING_FOR_EXPERT_SUGGESTION))
+            throw new NotFoundException("There is no order with such status!");
+
+        return repository.findAllByStatus(OrderStatus.WAITING_FOR_EXPERT_SUGGESTION);
+    }
+
+    @Override
     public Order updateStatusToStarted(long orderId) {
         Order byId = findById(orderId);
         byId.setStatus(OrderStatus.STARTED);
-        return repository.save(byId);
+        Order saved = repository.save(byId);
+        if (saved.getId()!=null)
+            reduce1ScoreFromExpertPerHour(saved.getId());
+
+        return saved;
     }
 
     @Override
@@ -63,4 +117,28 @@ public class OrderServiceImpl implements OrderService{
         byId.setStatus(OrderStatus.PAYED);
         return repository.save(byId);
     }
+
+    @Override
+    public void reduce1ScoreFromExpertPerHour(long orderId) {
+        Order order = findById(orderId);
+        if (!order.getToDoDateAndTime().isBefore(LocalDateTime.now()))
+            throw new IneligibleObjectException("Its not the orders date!");
+        if (!order.getStatus().equals(OrderStatus.WAITING_FOR_EXPERT_TO_VISIT))
+            throw new IneligibleObjectException("Order is not waiting for expert!");
+
+        long between = ChronoUnit.HOURS.between(order.getToDoDateAndTime(), LocalDateTime.now());
+        double newScore;
+        Expert byUsername = expertService.findByUsername(order.getSuggestion().getExpert().getUsername());
+        if (between>=1){
+            newScore = byUsername.getScore()-between;
+            if (newScore<0)
+                newScore = 0;
+            byUsername.setScore(newScore);
+        }
+        Expert updated = expertService.updateScore(byUsername);
+        if (updated.getScore().equals(0D))
+            expertService.updateStatusToUnverified(byUsername.getId());
+    }
+
+
 }
