cf56e236b397db20b0ecfa988d0220680d424987
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-30 20:45:49 +0330

    SuggestionRepository updated.

--- src/main/java/ir/maedehhz/final_project_spring/repository/SuggestionRepository.java
+++ src/main/java/ir/maedehhz/final_project_spring/repository/SuggestionRepository.java
@@ -1,9 +1,23 @@
 package ir.maedehhz.final_project_spring.repository;
 
+import ir.maedehhz.final_project_spring.model.Expert;
+import ir.maedehhz.final_project_spring.model.Order;
 import ir.maedehhz.final_project_spring.model.Suggestion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SuggestionRepository extends JpaRepository<Suggestion, Long> {
+    @Query("select s from Suggestion s order by s.expert.score desc ")
+    List<Suggestion> findAllByExpert_Score();
+
+    @Query("select s from Suggestion s order by s.price asc ")
+    List<Suggestion> findAllByPrice();
+
+    Suggestion findByExpertAndOrder(Expert expert, Order order);
+
+    boolean existsByExpertAndOrder(Expert expert, Order order);
 }
