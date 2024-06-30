0050b925c89c225c108c29173164838455e33423
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-30 20:40:12 +0330

    OrderStatus updated.

--- src/main/java/ir/maedehhz/final_project_spring/model/enums/OrderStatus.java
+++ src/main/java/ir/maedehhz/final_project_spring/model/enums/OrderStatus.java
@@ -2,7 +2,6 @@ package ir.maedehhz.final_project_spring.model.enums;
 
 public enum OrderStatus {
     WAITING_FOR_EXPERT_SUGGESTION,
-    WAITING_FOR_EXPERT_TO_TAKE,
     WAITING_FOR_EXPERT_TO_VISIT,
     STARTED,
     DONE,
