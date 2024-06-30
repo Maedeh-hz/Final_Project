9d93d2679098858f8822516dc12ac9e10e7e6b78
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-30 21:15:36 +0330

    User_SubserviceService updated.

--- src/main/java/ir/maedehhz/final_project_spring/service/user_subservice/User_SubserviceService.java
+++ src/main/java/ir/maedehhz/final_project_spring/service/user_subservice/User_SubserviceService.java
@@ -1,11 +1,14 @@
 package ir.maedehhz.final_project_spring.service.user_subservice;
 
+import ir.maedehhz.final_project_spring.model.Expert;
+import ir.maedehhz.final_project_spring.model.Subservice;
 import ir.maedehhz.final_project_spring.model.User_SubService;
 
 import java.util.List;
 
 public interface User_SubserviceService {
-    User_SubService save(User_SubService userSubService);
+    User_SubService save(long expertId, long subserviceId);
     boolean remove(long subserviceId, long expertId);
     List<User_SubService> findAllByExpert_Id(long expertId);
+    List<Expert> findAllBySubservice(long subserviceId);
 }
--- src/main/java/ir/maedehhz/final_project_spring/service/user_subservice/User_SubserviceServiceImpl.java
+++ src/main/java/ir/maedehhz/final_project_spring/service/user_subservice/User_SubserviceServiceImpl.java
@@ -3,29 +3,42 @@ package ir.maedehhz.final_project_spring.service.user_subservice;
 import ir.maedehhz.final_project_spring.exception.DuplicateInfoException;
 import ir.maedehhz.final_project_spring.exception.NotFoundException;
 import ir.maedehhz.final_project_spring.exception.UnverifiedExpertException;
+import ir.maedehhz.final_project_spring.model.Expert;
+import ir.maedehhz.final_project_spring.model.Subservice;
 import ir.maedehhz.final_project_spring.model.User_SubService;
 import ir.maedehhz.final_project_spring.model.enums.ExpertStatus;
 import ir.maedehhz.final_project_spring.repository.User_SubserviceRepository;
+import ir.maedehhz.final_project_spring.service.expert.ExpertServiceImpl;
+import ir.maedehhz.final_project_spring.service.subservice.SubserviceServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class User_SubserviceServiceImpl implements User_SubserviceService{
     private final User_SubserviceRepository repository;
+    private final SubserviceServiceImpl subserviceService;
+    private final ExpertServiceImpl expertService;
 
     @Override
-    public User_SubService save(User_SubService userSubService) {
-        if (repository.existsByExpertAndSubservice(userSubService.getExpert(), userSubService.getSubservice()))
+    public User_SubService save(long expertId, long subserviceId) {
+        Expert expert = expertService.findById(expertId);
+        Subservice subservice = subserviceService.findById(subserviceId);
+
+        User_SubService model = User_SubService.builder()
+                .subservice(subservice).expert(expert).build();
+
+        if (repository.existsByExpertAndSubservice(model.getExpert(), model.getSubservice()))
             throw new DuplicateInfoException(
                     "Expert has been added to this Subservice before!");
 
-        if (userSubService.getExpert().getStatus().equals(ExpertStatus.UNVERIFIED))
+        if (model.getExpert().getStatus().equals(ExpertStatus.UNVERIFIED))
             throw new UnverifiedExpertException("Expert is not verified!");
 
-        return repository.save(userSubService);
+        return repository.save(model);
     }
 
     @Override
@@ -41,6 +54,18 @@ public class User_SubserviceServiceImpl implements User_SubserviceService{
     @Override
     public List<User_SubService> findAllByExpert_Id(long expertId) {
         return repository.findAllByExpert_Id(expertId);
+    }
+
+    @Override
+    public List<Expert> findAllBySubservice(long subserviceId) {
+        Subservice subservice = subserviceService.findById(subserviceId);
+        List<User_SubService> all = repository.findAllBySubservice(subservice);
+        List<Expert> allExperts = new ArrayList<>();
+        all.forEach(userSubService -> allExperts.add(userSubService.getExpert()));
+        if (allExperts.isEmpty())
+            throw new NotFoundException("No Expert founded for this Subservice! ");
+
+        return allExperts;
     }
 
 }
