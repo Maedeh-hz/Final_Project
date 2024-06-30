3442c812362977c6751fe2762bc526c02d44537b
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-30 20:36:11 +0330

    ExpertRepository updated.

--- src/main/java/ir/maedehhz/final_project_spring/repository/ExpertRepository.java
+++ src/main/java/ir/maedehhz/final_project_spring/repository/ExpertRepository.java
@@ -4,14 +4,9 @@ import ir.maedehhz.final_project_spring.model.Expert;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ExpertRepository extends JpaRepository<Expert, Long> {
-    <S extends Expert> boolean existsByUsername(String username);
+    boolean existsByUsername(String username);
 
-    <S extends Expert, R> R findByUsername(String username);
-
-    @Override
-    Optional<Expert> findById(Long aLong);
+    Expert findByUsername(String username);
 }
