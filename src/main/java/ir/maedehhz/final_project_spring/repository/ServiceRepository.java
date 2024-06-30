890fea5bc8bca438f30c26829640e1bafc154014
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-30 20:42:00 +0330

    ServiceRepository updated.

--- src/main/java/ir/maedehhz/final_project_spring/repository/ServiceRepository.java
+++ src/main/java/ir/maedehhz/final_project_spring/repository/ServiceRepository.java
@@ -4,12 +4,7 @@ import ir.maedehhz.final_project_spring.model.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ServiceRepository extends JpaRepository<Service, Long> {
-    <S extends Service> boolean existsByServiceName(String serviceName);
-
-    @Override
-    List<Service> findAll();
+    boolean existsByServiceName(String serviceName);
 }
