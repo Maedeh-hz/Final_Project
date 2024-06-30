12a7da4a85226cdb07fe0b8c69171ddfee3ff90c
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-30 21:26:44 +0330

    Service DTOs created.

---
+++ src/main/java/ir/maedehhz/final_project_spring/dto/service/ServiceSaveRequest.java
@@ -0,0 +1,4 @@
+package ir.maedehhz.final_project_spring.dto.service;
+
+public record ServiceSaveRequest(String serviceName) {
+}
---
+++ src/main/java/ir/maedehhz/final_project_spring/dto/service/ServiceSaveResponse.java
@@ -0,0 +1,4 @@
+package ir.maedehhz.final_project_spring.dto.service;
+
+public record ServiceSaveResponse(long id, String serviceName) {
+}
