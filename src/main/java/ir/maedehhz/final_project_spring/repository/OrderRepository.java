661cb0023ea4ddefd4b0971e8a2125e15468df1c
Maede-hz <maedehosseinzade84@gmail.com>
2024-06-30 20:36:55 +0330

    OrderRepository updated.

--- src/main/java/ir/maedehhz/final_project_spring/repository/OrderRepository.java
+++ src/main/java/ir/maedehhz/final_project_spring/repository/OrderRepository.java
@@ -1,9 +1,18 @@
 package ir.maedehhz.final_project_spring.repository;
 
 import ir.maedehhz.final_project_spring.model.Order;
+import ir.maedehhz.final_project_spring.model.Subservice;
+import ir.maedehhz.final_project_spring.model.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllBySubservice(Subservice subservice);
+
+    List<Order> findAllByStatus(OrderStatus status);
+
+    boolean existsByStatus(OrderStatus status);
 }
