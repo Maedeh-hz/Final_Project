a27729a63095fedf5b2d831c1c82dc8b544eca8b
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-01 16:58:36 +0330

    OrderRepository updated.

--- src/main/java/ir/maedehhz/final_project_spring/repository/OrderRepository.java
+++ src/main/java/ir/maedehhz/final_project_spring/repository/OrderRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findAllBySubservice(Subservice subservice);
+    List<Order> findAllBySubservice_Id(Long subservice_id);
 
     List<Order> findAllByStatus(OrderStatus status);
 
