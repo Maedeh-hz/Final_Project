7e6c668b5f1d074d650c1b00b9c601b418f8c338
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-01 16:59:56 +0330

    OrderRepository updated.

--- src/main/java/ir/maedehhz/final_project_spring/repository/OrderRepository.java
+++ src/main/java/ir/maedehhz/final_project_spring/repository/OrderRepository.java
@@ -1,7 +1,6 @@
 package ir.maedehhz.final_project_spring.repository;
 
 import ir.maedehhz.final_project_spring.model.Order;
-import ir.maedehhz.final_project_spring.model.Subservice;
 import ir.maedehhz.final_project_spring.model.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
