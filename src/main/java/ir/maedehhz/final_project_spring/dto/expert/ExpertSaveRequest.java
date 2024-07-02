6e0be943583adb690232defbd1a1ed0abeb157ef
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-02 14:30:49 +0330

    Expert DTOs updated.

--- src/main/java/ir/maedehhz/final_project_spring/dto/expert/ExpertSaveRequest.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/expert/ExpertSaveRequest.java
@@ -3,6 +3,7 @@ package ir.maedehhz.final_project_spring.dto.expert;
 import ir.maedehhz.final_project_spring.model.Wallet;
 
 public record ExpertSaveRequest(String firstName, String lastName,
+                                String expertise,
                                 String email, String password,
                                 Wallet wallet, String imagePath) {
 }
--- src/main/java/ir/maedehhz/final_project_spring/dto/expert/ExpertSaveResponse.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/expert/ExpertSaveResponse.java
@@ -4,6 +4,7 @@ import ir.maedehhz.final_project_spring.model.Wallet;
 import ir.maedehhz.final_project_spring.model.enums.ExpertStatus;
 
 public record ExpertSaveResponse(long id, String firstName, String lastName,
+                                 String expertise,
                                  String email, String username,
                                  Wallet wallet, ExpertStatus status,
                                  Double score, byte[] image) {
