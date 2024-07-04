9bbf44db3199efcc443025bba861fbb7c2f3b23c
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-04 21:50:41 +0330

    Card entity created.

---
+++ src/main/java/ir/maedehhz/final_project_spring/model/Card.java
@@ -0,0 +1,41 @@
+package ir.maedehhz.final_project_spring.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SoftDelete;
+import org.hibernate.annotations.processing.Pattern;
+
+@Getter
+@Setter
+@SuperBuilder
+@SoftDelete
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String cardNumber;
+
+    private String cvv2;
+
+    private String expirationDate;
+
+    @Transient
+    private String captcha;
+
+    @Transient
+    private String hiddenCaptcha;
+
+    @Transient
+    private String realCaptcha;
+
+}
