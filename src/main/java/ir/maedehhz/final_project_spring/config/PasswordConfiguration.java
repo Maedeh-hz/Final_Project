2c35fca3e2efb0b219119737db8e93818fd4330c
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-14 19:42:44 +0330

    PasswordConfiguration class created.

---
+++ src/main/java/ir/maedehhz/final_project_spring/config/PasswordConfiguration.java
@@ -0,0 +1,14 @@
+package ir.maedehhz.final_project_spring.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//@Configuration
+public class PasswordConfiguration {
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+    }
+//}
