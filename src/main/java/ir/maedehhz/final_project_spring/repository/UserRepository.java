3c5fa3095b533f8bc28e92af6616a829fd443287
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-14 19:49:33 +0330

    UserRepository updated.

--- src/main/java/ir/maedehhz/final_project_spring/repository/UserRepository.java
+++ src/main/java/ir/maedehhz/final_project_spring/repository/UserRepository.java
@@ -1,14 +1,20 @@
 package ir.maedehhz.final_project_spring.repository;
 
 import ir.maedehhz.final_project_spring.model.User;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> , JpaSpecificationExecutor<User> {
+    Optional<User> findByUsername(String username);
+
     List<User> findAllByDtype(String dtype);
 
     List<User> findAllByFirstNameLike(String firstName);
