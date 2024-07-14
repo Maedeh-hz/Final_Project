db3887313c3a0971c63653ab34a4431c904cf10f
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-14 22:17:07 +0330

    SecurityConfiguration updated.

--- src/main/java/ir/maedehhz/final_project_spring/config/SecurityConfiguration.java
+++ src/main/java/ir/maedehhz/final_project_spring/config/SecurityConfiguration.java
@@ -1,49 +1,52 @@
 package ir.maedehhz.final_project_spring.config;
-//
-//import ir.maedehhz.final_project_spring.service.user.UserServiceImpl;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//@EnableMethodSecurity
-//@RequiredArgsConstructor
+
+import ir.maedehhz.final_project_spring.service.user.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
-//
-//    private final UserServiceImpl userService;
-//    private final BCryptPasswordEncoder passwordEncoder;
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .cors(AbstractHttpConfigurer::disable)
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(
-//                        s -> s.anyRequest().permitAll()
-//                )
-//                .httpBasic(Customizer.withDefaults())
-//        ;
-//
-//        return http.build();
-//    }
-//
-//    @Autowired
-//    public void configureBuild(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .userDetailsService(userService::findByUsername)
-//                .passwordEncoder(passwordEncoder);
-//    }
-//
-//
+
+    private final UserServiceImpl userService;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        s -> s
+//                                .requestMatchers(HttpMethod.POST, "/api/v1/expert/save-expert")
+//                                .permitAll()
+                                .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+        ;
+
+        return http.build();
+    }
+
+    @Autowired
+    public void configureBuild(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userService::findByUsername)
+                .passwordEncoder(passwordEncoder);
+    }
+
+
 }
