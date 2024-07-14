6f3ee6fbb84a38a6c1bf1921043bad2fd15919b1
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-14 19:52:04 +0330

    CustomerRepository updated.

--- src/main/java/ir/maedehhz/final_project_spring/repository/CustomerRepository.java
+++ src/main/java/ir/maedehhz/final_project_spring/repository/CustomerRepository.java
@@ -1,11 +1,18 @@
 package ir.maedehhz.final_project_spring.repository;
 
 import ir.maedehhz.final_project_spring.model.Customer;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.function.Function;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
     boolean existsByUsername(String username);
+
+    Optional<Customer> findByUsername(String username);
 }
