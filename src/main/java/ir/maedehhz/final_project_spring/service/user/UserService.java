b47ec1fa87ca8d48cb3cbfafd58ac14dd9b6508c
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-14 19:50:44 +0330

    UserService class + interface updated.

--- src/main/java/ir/maedehhz/final_project_spring/service/user/UserService.java
+++ src/main/java/ir/maedehhz/final_project_spring/service/user/UserService.java
@@ -1,4 +1,7 @@
 package ir.maedehhz.final_project_spring.service.user;
 
+import ir.maedehhz.final_project_spring.model.User;
+
 public interface UserService {
+    User findByUsername(String username);
 }
--- src/main/java/ir/maedehhz/final_project_spring/service/user/UserServiceImpl.java
+++ src/main/java/ir/maedehhz/final_project_spring/service/user/UserServiceImpl.java
@@ -1,7 +1,20 @@
 package ir.maedehhz.final_project_spring.service.user;
 
+import ir.maedehhz.final_project_spring.exception.NotFoundException;
+import ir.maedehhz.final_project_spring.model.User;
+import ir.maedehhz.final_project_spring.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository repository;
+
+    @Override
+    public User findByUsername(String username) {
+        return repository.findByUsername(username).orElseThrow(() ->
+                new NotFoundException(String.format("No user with username %s found!", username)));
+    }
 }
