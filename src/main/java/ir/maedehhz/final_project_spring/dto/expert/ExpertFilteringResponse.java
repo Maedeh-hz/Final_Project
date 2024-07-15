9462213424f858370dfb3661668d5dde0135ea03
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-15 18:48:51 +0330

    ExpertFilteringResponse created.

---
+++ src/main/java/ir/maedehhz/final_project_spring/dto/expert/ExpertFilteringResponse.java
@@ -0,0 +1,9 @@
+package ir.maedehhz.final_project_spring.dto.expert;
+
+import ir.maedehhz.final_project_spring.model.Wallet;
+import ir.maedehhz.final_project_spring.model.enums.ExpertStatus;
+
+public record ExpertFilteringResponse (long id, String firstName, String lastName,
+                                       String expertise, String username,
+                                       ExpertStatus status, Double score) {
+}
