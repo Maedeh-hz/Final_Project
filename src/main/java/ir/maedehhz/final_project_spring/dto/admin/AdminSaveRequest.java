1b55c3c089f8ed1b79f73284d8fd57f0027c71ec
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-16 18:23:04 +0330

    AdminSaveRequest and AdminSaveResponse updated.

--- src/main/java/ir/maedehhz/final_project_spring/dto/admin/AdminSaveRequest.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/admin/AdminSaveRequest.java
@@ -3,6 +3,5 @@ package ir.maedehhz.final_project_spring.dto.admin;
 import ir.maedehhz.final_project_spring.model.Wallet;
 
 public record AdminSaveRequest(String firstName, String lastName,
-                               String email, String username, String password,
-                               Wallet wallet){
+                               String email, String username, String password){
 }
--- src/main/java/ir/maedehhz/final_project_spring/dto/admin/AdminSaveResponse.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/admin/AdminSaveResponse.java
@@ -4,7 +4,8 @@ import ir.maedehhz.final_project_spring.model.Wallet;
 
 import java.time.LocalDate;
 
-public record AdminSaveResponse(long id, String firstName, String lastName, String email, String username,
-                                Wallet wallet, LocalDate registrationDate) {
+public record AdminSaveResponse(long id, String firstName, String lastName,
+                                String email, String username,
+                                LocalDate registrationDate) {
 
 }
