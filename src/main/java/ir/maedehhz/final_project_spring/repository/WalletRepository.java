02ac02408f744cabd0cce051cfe876f8258ebd10
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-16 18:26:13 +0330

    WalletRepository updated.

--- src/main/java/ir/maedehhz/final_project_spring/repository/WalletRepository.java
+++ src/main/java/ir/maedehhz/final_project_spring/repository/WalletRepository.java
@@ -1,9 +1,16 @@
 package ir.maedehhz.final_project_spring.repository;
 
+import ir.maedehhz.final_project_spring.model.User;
 import ir.maedehhz.final_project_spring.model.Wallet;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.function.Function;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    Optional<Wallet> findByUser(User user);
 }
