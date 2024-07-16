9c35462f416e62bed31c1c9bd8414d1707e76d9e
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-16 18:24:16 +0330

    ExpertFilteringResponse and ExpertSaveRequest updated.

--- src/main/java/ir/maedehhz/final_project_spring/dto/expert/ExpertFilteringResponse.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/expert/ExpertFilteringResponse.java
@@ -2,7 +2,9 @@ package ir.maedehhz.final_project_spring.dto.expert;
 
 import ir.maedehhz.final_project_spring.model.enums.ExpertStatus;
 
+import java.time.LocalDateTime;
+
 public record ExpertFilteringResponse (long id, String firstName, String lastName,
                                        String expertise, String username,
-                                       ExpertStatus status, Double score) {
+                                       ExpertStatus status, Double score, LocalDateTime registrationDate) {
 }
--- src/main/java/ir/maedehhz/final_project_spring/dto/expert/ExpertSaveRequest.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/expert/ExpertSaveRequest.java
@@ -1,9 +1,6 @@
 package ir.maedehhz.final_project_spring.dto.expert;
 
-import ir.maedehhz.final_project_spring.model.Wallet;
-
 public record ExpertSaveRequest(String firstName, String lastName,
                                 String expertise,
-                                String email, String password,
-                                Wallet wallet, String imagePath) {
+                                String email, String password, String imagePath) {
 }
