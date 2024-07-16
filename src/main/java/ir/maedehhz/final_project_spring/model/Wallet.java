3baf33034f46088e4a29bff545a6c9e3a4143f6e
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-16 18:21:49 +0330

    Wallet entity updated.

--- src/main/java/ir/maedehhz/final_project_spring/model/Wallet.java
+++ src/main/java/ir/maedehhz/final_project_spring/model/Wallet.java
@@ -2,10 +2,14 @@ package ir.maedehhz.final_project_spring.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SoftDelete;
 
+@Getter
+@Setter
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,4 +23,7 @@ public class Wallet {
     private Long id;
 
     private Double balance;
+
+    @OneToOne
+    private User user;
 }
