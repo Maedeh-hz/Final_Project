f715f372568609373c88c7a461e259ada1153db6
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-16 18:27:34 +0330

    WalletService class + interface updated.

--- src/main/java/ir/maedehhz/final_project_spring/service/wallet/WalletService.java
+++ src/main/java/ir/maedehhz/final_project_spring/service/wallet/WalletService.java
@@ -1,4 +1,16 @@
 package ir.maedehhz.final_project_spring.service.wallet;
 
+import ir.maedehhz.final_project_spring.model.Order;
+import ir.maedehhz.final_project_spring.model.User;
+import ir.maedehhz.final_project_spring.model.Wallet;
+
 public interface WalletService {
+    void register(User user);
+
+    Wallet findByUserId(long userId);
+
+    Wallet payingFromWallet(Order order);
+
+    Wallet showWalletToUser(long userId);
+
 }
--- src/main/java/ir/maedehhz/final_project_spring/service/wallet/WalletServiceImpl.java
+++ src/main/java/ir/maedehhz/final_project_spring/service/wallet/WalletServiceImpl.java
@@ -1,7 +1,61 @@
 package ir.maedehhz.final_project_spring.service.wallet;
 
+import ir.maedehhz.final_project_spring.exception.CouldNotUpdateException;
+import ir.maedehhz.final_project_spring.exception.NotFoundException;
+import ir.maedehhz.final_project_spring.model.Order;
+import ir.maedehhz.final_project_spring.model.User;
+import ir.maedehhz.final_project_spring.model.Wallet;
+import ir.maedehhz.final_project_spring.repository.WalletRepository;
+import ir.maedehhz.final_project_spring.service.order.OrderServiceImpl;
+import ir.maedehhz.final_project_spring.service.user.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class WalletServiceImpl implements WalletService{
+
+    private final WalletRepository repository;
+    private final UserServiceImpl userService;
+
+    @Override
+    public void register(User user) {
+        Wallet wallet = Wallet.builder().balance(0D).user(user).build();
+
+        repository.save(wallet);
+    }
+
+    @Override
+    public Wallet findByUserId(long userId) {
+        User user = userService.findById(userId);
+        return repository.findByUser(user).orElseThrow(() -> new NotFoundException(
+                String.format("Wallet with user id %s not found!", userId)
+        ));
+    }
+
+    @Override
+    public Wallet payingFromWallet(Order order) {
+        Long customerId = order.getCustomer().getId();
+        Wallet wallet = findByUserId(customerId);
+        Double ordersPrice = order.getSuggestion().getPrice();
+        
+        if (wallet.getBalance() < ordersPrice)
+            throw new CouldNotUpdateException("Insufficient credit!");
+        
+        double newBalance = wallet.getBalance() - ordersPrice;
+        wallet.setBalance(newBalance);
+        Wallet saved = repository.save(wallet);
+
+        double expertShare = (ordersPrice * 100) / 70;
+        Wallet expertWallet = findByUserId(order.getSuggestion().getExpert().getId());
+        expertWallet.setBalance(expertWallet.getBalance()+expertShare);
+        repository.save(expertWallet);
+
+        return saved;
+    }
+
+    @Override
+    public Wallet showWalletToUser(long userId) {
+        return findByUserId(userId);
+    }
 }
