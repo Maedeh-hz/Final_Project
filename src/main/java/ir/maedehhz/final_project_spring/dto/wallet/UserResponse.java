cfa772a1d698087f601d105c3fcdb564d21efaa9
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-16 18:24:38 +0330

    UserResponse record created.

---
+++ src/main/java/ir/maedehhz/final_project_spring/dto/wallet/UserResponse.java
@@ -0,0 +1,5 @@
+package ir.maedehhz.final_project_spring.dto.wallet;
+
+public record UserResponse(Long id, String username, String firstName,
+                           String lastName, String dtype) {
+}
