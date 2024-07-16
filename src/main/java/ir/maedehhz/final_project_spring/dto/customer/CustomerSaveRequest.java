686e307013bc421b5077b16175c06f444a7067db
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-16 18:23:49 +0330

    CustomerSaveRequest and CustomerSaveResponse updated.

--- src/main/java/ir/maedehhz/final_project_spring/dto/customer/CustomerSaveRequest.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/customer/CustomerSaveRequest.java
@@ -1,8 +1,5 @@
 package ir.maedehhz.final_project_spring.dto.customer;
 
-import ir.maedehhz.final_project_spring.model.Wallet;
-
 public record CustomerSaveRequest(String firstName, String lastName,
-                                  String email, String password,
-                                  Wallet wallet) {
+                                  String email, String password) {
 }
--- src/main/java/ir/maedehhz/final_project_spring/dto/customer/CustomerSaveResponse.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/customer/CustomerSaveResponse.java
@@ -4,6 +4,7 @@ import ir.maedehhz.final_project_spring.model.Wallet;
 
 import java.time.LocalDate;
 
-public record CustomerSaveResponse(long id, String firstName, String lastName, String email, String username,
+public record CustomerSaveResponse(long id, String firstName, String lastName,
+                                   String email, String username,
                                    Wallet wallet, LocalDate registrationDate) {
 }
