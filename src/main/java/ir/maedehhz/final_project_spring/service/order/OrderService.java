ae087c9e1cfde26837daf2e8e502e44f52c67a9e
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-18 00:57:54 +0330

    OrderService class + interface updated.

--- src/main/java/ir/maedehhz/final_project_spring/service/order/OrderService.java
+++ src/main/java/ir/maedehhz/final_project_spring/service/order/OrderService.java
@@ -1,5 +1,6 @@
 package ir.maedehhz.final_project_spring.service.order;
 
+import ir.maedehhz.final_project_spring.dto.order.OrderFilteringResponse;
 import ir.maedehhz.final_project_spring.dto.order.OrderFindAllResponse;
 import ir.maedehhz.final_project_spring.model.Order;
 
@@ -7,14 +8,19 @@ import java.util.List;
 
 public interface OrderService {
     Order save(Order order, long subserviceId, long customerId);
+    Order choosingExpert(long suggestionId);
     Order findById(long id);
-    Order updatingOrderSuggestion(Order order);
     Order updateStatusToWaitingForExpertToVisit(long orderId);
     List<OrderFindAllResponse> findAllBySubserviceWaitingForExpertSuggestion(long subserviceId);
     List<Order> findAllByStatusWaitingForSuggestion();
     Order updateStatusToStarted(long orderId);
     Order updateStatusToDone(long orderId);
-    Order updateStatusToPayed(long orderId);
+    void updateStatusToPayed(long orderId);
     void reduce1ScoreFromExpertPerHour(long orderId);
+
+    List<OrderFilteringResponse> filteringOrders(
+            Long userId, String registrationDate, String orderStatus,
+            Long serviceId, Long subserviceId
+    );
 
 }
--- src/main/java/ir/maedehhz/final_project_spring/service/order/OrderServiceImpl.java
+++ src/main/java/ir/maedehhz/final_project_spring/service/order/OrderServiceImpl.java
@@ -1,18 +1,25 @@
 package ir.maedehhz.final_project_spring.service.order;
 
+import ir.maedehhz.final_project_spring.dto.order.OrderFilteringResponse;
 import ir.maedehhz.final_project_spring.dto.order.OrderFindAllResponse;
+import ir.maedehhz.final_project_spring.exception.CouldNotUpdateException;
 import ir.maedehhz.final_project_spring.exception.DateMismatchException;
 import ir.maedehhz.final_project_spring.exception.NotFoundException;
 import ir.maedehhz.final_project_spring.mapper.order.OrderMapper;
-import ir.maedehhz.final_project_spring.model.Customer;
-import ir.maedehhz.final_project_spring.model.Expert;
-import ir.maedehhz.final_project_spring.model.Order;
-import ir.maedehhz.final_project_spring.model.Subservice;
+import ir.maedehhz.final_project_spring.model.*;
 import ir.maedehhz.final_project_spring.model.enums.OrderStatus;
 import ir.maedehhz.final_project_spring.repository.OrderRepository;
 import ir.maedehhz.final_project_spring.service.customer.CustomerServiceImpl;
 import ir.maedehhz.final_project_spring.service.expert.ExpertServiceImpl;
+import ir.maedehhz.final_project_spring.service.service.ServiceServiceImpl;
 import ir.maedehhz.final_project_spring.service.subservice.SubserviceServiceImpl;
+import ir.maedehhz.final_project_spring.service.suggestion.SuggestionServiceImpl;
+import ir.maedehhz.final_project_spring.service.user.UserServiceImpl;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +27,7 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
@@ -27,11 +35,19 @@ public class OrderServiceImpl implements OrderService{
 
     private final OrderRepository repository;
 
+    private final UserServiceImpl userService;
+
     private final ExpertServiceImpl expertService;
+
+    private final ServiceServiceImpl serviceService;
 
     private final SubserviceServiceImpl subserviceService;
 
     private final CustomerServiceImpl customerService;
+
+    private final SuggestionServiceImpl suggestionService;
+
+    private final EntityManager entityManager;
 
     @Override
     public Order save(Order order, long subserviceId, long customerId) {
@@ -45,18 +61,37 @@ public class OrderServiceImpl implements OrderService{
 
         order.setRegisterDate(LocalDateTime.now());
         order.setStatus(OrderStatus.WAITING_FOR_EXPERT_SUGGESTION);
+        order.setService(subservice.getService());
         return repository.save(order);
+    }
+
+    @Override
+    public Order choosingExpert(long suggestionId) {
+        Order order = updatingOrdersSuggestion(suggestionId);
+
+        if (order.getExpert() == null)
+            throw new CouldNotUpdateException("couldn't register the suggestion for order!");
+
+        return updateStatusToWaitingForExpertToVisit(order.getId());
+    }
+
+    private Order updatingOrdersSuggestion(Long suggestionId){
+        Suggestion suggestion = suggestionService.findById(suggestionId);
+
+        Order orderById = findById(suggestion.getOrder().getId());
+
+        if (orderById.getExpert() != null)
+            throw new CouldNotUpdateException("You have already chose Expert for your order!");
+
+        orderById.setExpert(suggestion.getExpert());
+
+        return repository.save(orderById);
     }
 
     @Override
     public Order findById(long id) {
         return repository.findById(id).orElseThrow(() ->
                 new NotFoundException(String.format("Order with id %s couldn't be found.", id)));
-    }
-
-    @Override
-    public Order updatingOrderSuggestion(Order order) {
-        return repository.save(order);
     }
 
     @Override
@@ -108,10 +143,10 @@ public class OrderServiceImpl implements OrderService{
     }
 
     @Override
-    public Order updateStatusToPayed(long orderId) {
+    public void updateStatusToPayed(long orderId) {
         Order byId = findById(orderId);
         byId.setStatus(OrderStatus.PAYED);
-        return repository.save(byId);
+        repository.save(byId);
     }
 
     @Override
@@ -122,7 +157,7 @@ public class OrderServiceImpl implements OrderService{
 
         long between = ChronoUnit.HOURS.between(order.getToDoDateAndTime(), LocalDateTime.now());
         double newScore;
-        Expert byUsername = expertService.findByUsername(order.getSuggestion().getExpert().getUsername());
+        Expert byUsername = expertService.findByEmail(order.getExpert().getUsername());
         if (between>=1){
             newScore = byUsername.getScore()-between;
             if (newScore<0)
@@ -132,6 +167,55 @@ public class OrderServiceImpl implements OrderService{
         Expert updated = expertService.updateScore(byUsername);
         if (updated.getScore().equals(0D))
             expertService.updateStatusToUnverified(byUsername.getId());
+    }
+
+    @Override
+    public List<OrderFilteringResponse> filteringOrders(Long userId,
+                                                        String registrationDate,
+                                                        String orderStatus,
+                                                        Long serviceId, Long subserviceId
+    ) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+
+        Root<Order> root = cq.from(Order.class);
+        List<Predicate> predicates = new ArrayList<>();
+
+        if (userId != null){
+            User user = userService.findById(userId);
+            checkUsersType(user, predicates, cb, root);
+        }
+
+        if (registrationDate != null)
+            predicates.add(cb.like(root.get("registerDate"), registrationDate));
+
+        if (orderStatus != null)
+            predicates.add(cb.equal(root.get("status"), orderStatus.toUpperCase(Locale.ROOT)));
+
+        if (subserviceId != null){
+            Subservice subservice = subserviceService.findById(subserviceId);
+            predicates.add(cb.equal(root.get("subservice"), subservice));
+        }
+
+        if (serviceId != null) {
+            ir.maedehhz.final_project_spring.model.Service service = serviceService.findById(serviceId);
+            predicates.add(cb.equal(root.get("service"), service));
+        }
+
+        cq.where(predicates.toArray(new Predicate[0]));
+        List<Order> resultList = entityManager.createQuery(cq).getResultList();
+        List<OrderFilteringResponse> responses = new ArrayList<>();
+        resultList.forEach(order -> responses.add(OrderMapper.INSTANCE.modelToOrderFilteringResponse(order)));
+        return responses;
+    }
+
+    private static void checkUsersType(User user, List<Predicate> predicates, CriteriaBuilder cb, Root<Order> root) {
+        if (user.getDtype().equals("Customer"))
+            predicates.add(cb.equal(root.get("customer"), user));
+
+        if (user.getDtype().equals("Expert")){
+            predicates.add(cb.equal(root.get("expert"), user));
+        }
     }
 
 
