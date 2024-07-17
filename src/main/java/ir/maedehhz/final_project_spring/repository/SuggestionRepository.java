d1e3db3cc1917e4af76e63a380bce78ccc06c0c7
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-18 00:56:39 +0330

    SuggestionRepository updated.

--- src/main/java/ir/maedehhz/final_project_spring/repository/SuggestionRepository.java
+++ src/main/java/ir/maedehhz/final_project_spring/repository/SuggestionRepository.java
@@ -3,15 +3,12 @@ package ir.maedehhz.final_project_spring.repository;
 import ir.maedehhz.final_project_spring.model.Expert;
 import ir.maedehhz.final_project_spring.model.Order;
 import ir.maedehhz.final_project_spring.model.Suggestion;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 
 @Repository
 public interface SuggestionRepository extends JpaRepository<Suggestion, Long> {
