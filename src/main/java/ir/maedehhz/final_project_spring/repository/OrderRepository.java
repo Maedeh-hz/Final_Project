c67177361476e79640496576e4dfadded5c9f6a6
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-18 02:19:37 +0330

    OrderRepository updated.

--- src/main/java/ir/maedehhz/final_project_spring/repository/OrderRepository.java
+++ src/main/java/ir/maedehhz/final_project_spring/repository/OrderRepository.java
@@ -3,6 +3,7 @@ package ir.maedehhz.final_project_spring.repository;
 import ir.maedehhz.final_project_spring.model.Order;
 import ir.maedehhz.final_project_spring.model.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +12,8 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findAllBySubservice_IdAndStatus(Long subservice_id, OrderStatus status);
 
-    List<Order> findAllByStatus(OrderStatus status);
+    @Query("select o from Order o where o.expert.id = ?1 and o.status='DONE'")
+    List<Order> findAllByExpert_Id(Long expert_id);
 
-    boolean existsByStatus(OrderStatus status);
+    List<Order> findAllByCustomer_Id(Long customer_id);
 }
