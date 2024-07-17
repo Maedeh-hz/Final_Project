7ecf88fe523edc7d78db0161a8f957ab111e3e8c
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-18 00:56:27 +0330

    ExpertRepository updated.

--- src/main/java/ir/maedehhz/final_project_spring/repository/ExpertRepository.java
+++ src/main/java/ir/maedehhz/final_project_spring/repository/ExpertRepository.java
@@ -1,12 +1,10 @@
 package ir.maedehhz.final_project_spring.repository;
 
 import ir.maedehhz.final_project_spring.model.Expert;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,8 +13,4 @@ public interface ExpertRepository extends JpaRepository<Expert, Long>, JpaSpecif
     boolean existsByEmail(String email);
 
     Optional<Expert> findByEmail(String email);
-
-    List<Expert> findAllByExpertiseLike(String expertise);
-
-    List<Expert> findAllByScore(Double score);
 }
