c69f45e053bec66f53e3fcddb295c7a2a8fb22c9
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-18 02:18:18 +0330

    CommentSaveResponse updated.

--- src/main/java/ir/maedehhz/final_project_spring/dto/comment/CommentSaveResponse.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/comment/CommentSaveResponse.java
@@ -1,9 +1,9 @@
 package ir.maedehhz.final_project_spring.dto.comment;
 
-import ir.maedehhz.final_project_spring.model.Order;
+import ir.maedehhz.final_project_spring.dto.order.OrderFindAllResponse;
 
 import java.time.LocalDateTime;
 
 public record CommentSaveResponse (long id ,String context, Double expertScore,
-                                   LocalDateTime registrationDate, Order order) {
+                                   LocalDateTime registrationDate, OrderFindAllResponse order) {
 }
