a3033ec4fc776f7ff7b9820d118dc98096501f5a
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-18 00:53:53 +0330

    OrderMapper updated.

--- src/main/java/ir/maedehhz/final_project_spring/mapper/order/OrderMapper.java
+++ src/main/java/ir/maedehhz/final_project_spring/mapper/order/OrderMapper.java
@@ -1,10 +1,12 @@
 package ir.maedehhz.final_project_spring.mapper.order;
 
+import ir.maedehhz.final_project_spring.dto.order.OrderFilteringResponse;
 import ir.maedehhz.final_project_spring.dto.order.OrderFindAllResponse;
 import ir.maedehhz.final_project_spring.dto.order.OrderSaveRequest;
 import ir.maedehhz.final_project_spring.dto.order.OrderSaveResponse;
 import ir.maedehhz.final_project_spring.model.Order;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -12,7 +14,16 @@ public interface OrderMapper {
     OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
 
     Order orderSaveRequestToModel(OrderSaveRequest request);
+
     OrderSaveResponse modelToOrderSaveResponse(Order order);
 
     OrderFindAllResponse modelToOrderFindAllResponse(Order order);
+
+    @Mapping(source = "address.province", target = "addressProvince")
+    @Mapping(source = "address.id", target = "addressId")
+    @Mapping(source = "expert.id", target = "expertId")
+    @Mapping(source = "customer.id", target = "customerId")
+    @Mapping(source = "service.id", target = "serviceId")
+    @Mapping(source = "subservice.id", target = "subserviceId")
+    OrderFilteringResponse modelToOrderFilteringResponse(Order order);
 }
