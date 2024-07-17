08ef792f481fb1d2657a0419e0bad327fb6f1c1c
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-18 00:51:52 +0330

    Suggestion DTOs updated.

--- src/main/java/ir/maedehhz/final_project_spring/dto/suggestion/SuggestionSaveRequest.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/suggestion/SuggestionSaveRequest.java
@@ -1,6 +1,5 @@
 package ir.maedehhz.final_project_spring.dto.suggestion;
 
-public record SuggestionSaveRequest(long expertId,
-                                    long orderId, String description,
+public record SuggestionSaveRequest(long expertId, long orderId, String description,
                                     Double price, Double workDuration) {
 }
--- src/main/java/ir/maedehhz/final_project_spring/dto/suggestion/SuggestionSaveResponse.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/suggestion/SuggestionSaveResponse.java
@@ -1,11 +1,8 @@
 package ir.maedehhz.final_project_spring.dto.suggestion;
 
-import ir.maedehhz.final_project_spring.model.Expert;
-import ir.maedehhz.final_project_spring.model.Order;
-
 import java.time.LocalDate;
 
-public record SuggestionSaveResponse(Expert expert, Order order,
+public record SuggestionSaveResponse(Long expertId,
                                      String description, LocalDate registerDate,
                                      Double price, Double workDuration) {
 }
