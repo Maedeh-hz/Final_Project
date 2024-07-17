e4b9bda6a1a8c0bfcce54952e0d8aa56202bdb27
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-18 02:18:47 +0330

    OrderSaveResponse updated.

--- src/main/java/ir/maedehhz/final_project_spring/dto/order/OrderSaveResponse.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/order/OrderSaveResponse.java
@@ -1,11 +1,13 @@
 package ir.maedehhz.final_project_spring.dto.order;
 
+import ir.maedehhz.final_project_spring.dto.customer.CustomerSaveResponse;
+import ir.maedehhz.final_project_spring.dto.expert.ExpertSaveResponse;
 import ir.maedehhz.final_project_spring.model.enums.OrderStatus;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public record OrderSaveResponse(Long subserviceId, Long customerId,
+public record OrderSaveResponse(ExpertSaveResponse expert, CustomerSaveResponse customer,
                                 String description, Double suggestingPrice,
                                 LocalDate registerDate, LocalDateTime toDoDateAndTime,
                                 OrderStatus status, Long addressId) {
