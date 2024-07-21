3a66060593273917a17b7bcb311fb9b4232b4ebd
Maede-hz <maedehosseinzade84@gmail.com>
2024-07-21 12:39:37 +0330

    OrderSaveRequest DTO updated.

--- src/main/java/ir/maedehhz/final_project_spring/dto/order/OrderSaveRequest.java
+++ src/main/java/ir/maedehhz/final_project_spring/dto/order/OrderSaveRequest.java
@@ -2,7 +2,7 @@ package ir.maedehhz.final_project_spring.dto.order;
 
 import java.time.LocalDateTime;
 
-public record OrderSaveRequest(Long subserviceId, Long customerId, String description,
+public record OrderSaveRequest(Long subserviceId, String description,
                                Double suggestingPrice, LocalDateTime toDoDateAndTime,
                                Long addressId) {
 }
